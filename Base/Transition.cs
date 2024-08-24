cd7ad3ced00fcbe1c05e18a3bc2116d71005e46c
0.0.0.0 <aavvkaaa2@mail.ru>
2024-08-25 00:13:24 +0900

    Transition

---
+++ Base/Transition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TresNight.Base
+{
+    internal class Transition
+    {
+        public static Frame frm0bj;
+    }
+}
